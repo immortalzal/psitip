3f89dcf58c83bee9a6bc2b7bd75d82f9f2f03232
immortalzal <tsealvina0@gmail.com>
2024-07-20 15:41:19 +0800

    Update psitip.py

--- psitip.py
+++ psitip.py
@@ -193,6 +193,11 @@ except ImportError:
     lark = None
     lark_v_args = lambda inline: (lambda x: x)
     lark_Transformer = object
+ 
+ try: 
+     import pycddlib
+except ImportError:
+     pycddlib = None
 
 
 # try:
